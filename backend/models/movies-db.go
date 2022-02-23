73da9c5dd7c3fe35fcf1f47ba3c43db4b2e91223
Leandro Bevilaqua <le.bevilaqua@gmail.com>
2022-02-22 21:14:26 -0300

    refac: movies db

--- backend/models/movies-db.go
+++ backend/models/movies-db.go
@@ -19,11 +19,9 @@ func (m *DBModel) GetById(id int) (*Movie, error) {
 			  from movies
 			  where id = $1
 			 `
-
 	row := m.DB.QueryRowContext(ctx, queryMovie, id)
 
 	var movie Movie
-
 	err := row.Scan(
 		&movie.Id,
 		&movie.Title,
@@ -40,13 +38,73 @@ func (m *DBModel) GetById(id int) (*Movie, error) {
 		return nil, err
 	}
 
+	genres, err := m.getMovieGenres(id, ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	movie.MovieGenre = genres
+
+	return &movie, nil
+}
+
+func (m *DBModel) GetAll() ([]*Movie, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	queryMovies := `select 
+						id, title, description, year, release_date, rating, runtime, mpaa_rating, created_at, updated_at 
+					from movies order by title
+			 		`
+
+	rows, err := m.DB.QueryContext(ctx, queryMovies)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	movies := make([]*Movie, 0)
+	for rows.Next() {
+
+		var movie Movie
+		err := rows.Scan(
+			&movie.Id,
+			&movie.Title,
+			&movie.Description,
+			&movie.Year,
+			&movie.ReleaseDate,
+			&movie.Rating,
+			&movie.Runtime,
+			&movie.MPAARating,
+			&movie.CreatedAt,
+			&movie.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		genres, err := m.getMovieGenres(movie.Id, ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		movie.MovieGenre = genres
+
+		movies = append(movies, &movie)
+	}
+
+	return movies, nil
+}
+
+func (m *DBModel) getMovieGenres(id int, ctx context.Context) (map[int]string, error) {
+
 	queryGenres := `select 
-				a.id, a.movie_id, a.genre_id, b.genre_name name, a.created_at, a.updated_at
-			from movies_genres a
-			left join genres b on (a.genre_id = b.id)
-			where 
-				a.movie_id = $1
-			`
+						a.id, a.movie_id, a.genre_id, b.genre_name name, a.created_at, a.updated_at
+					from movies_genres a
+					left join genres b on (a.genre_id = b.id)
+					where 
+						a.movie_id = $1
+					`
 
 	rows, err := m.DB.QueryContext(ctx, queryGenres, id)
 	if err != nil {
@@ -74,84 +132,5 @@ func (m *DBModel) GetById(id int) (*Movie, error) {
 		genres[mg.Id] = mg.Genre.Name
 	}
 
-	movie.MovieGenre = genres
-
-	return &movie, nil
-}
-
-func (m *DBModel) GetAll() ([]*Movie, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	queryMovies := `select 
-				id, title, description, year, release_date, rating, runtime, mpaa_rating, created_at, updated_at 
-				from movies order by title
-			 `
-
-	rows, err := m.DB.QueryContext(ctx, queryMovies)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	movies := make([]*Movie, 0)
-	for rows.Next() {
-		var movie Movie
-
-		err := rows.Scan(
-			&movie.Id,
-			&movie.Title,
-			&movie.Description,
-			&movie.Year,
-			&movie.ReleaseDate,
-			&movie.Rating,
-			&movie.Runtime,
-			&movie.MPAARating,
-			&movie.CreatedAt,
-			&movie.UpdatedAt,
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		queryGenres := `select 
-					a.id, a.movie_id, a.genre_id, b.genre_name name, a.created_at, a.updated_at
-				from movies_genres a
-				left join genres b on (a.genre_id = b.id)
-				where 
-					a.movie_id = $1
-				`
-
-		rowsGenre, err := m.DB.QueryContext(ctx, queryGenres, &movie.Id)
-		if err != nil {
-			return nil, err
-		}
-
-		genres := make(map[int]string, 0)
-		for rowsGenre.Next() {
-			var mg MovieGenre
-
-			err := rowsGenre.Scan(
-				&mg.Id,
-				&mg.MovieId,
-				&mg.GenreId,
-				&mg.Genre.Name,
-				&mg.CreatedAt,
-				&mg.UpdatedAt,
-			)
-
-			if err != nil {
-				return nil, err
-			}
-
-			genres[mg.Id] = mg.Genre.Name
-		}
-		rowsGenre.Close()
-
-		movie.MovieGenre = genres
-
-		movies = append(movies, &movie)
-	}
-
-	return movies, nil
+	return genres, nil
 }
